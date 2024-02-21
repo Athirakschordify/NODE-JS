a7740d8f767237d1183967034b720c077f04b6de
Athirakschordify <athira.ks@chordify.com>
2024-02-21 17:02:05 +0530

    addition of two numbers

---
+++ addition_of_two_numbers.js
@@ -0,0 +1,14 @@
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Input numbers
+const number1 = 5;
+const number2 = 3;
+
+// Perform addition
+const sum = addNumbers(number1, number2);
+
+// Print the result
+console.log(`The sum of ${number1} and ${number2} is: ${sum}`);
